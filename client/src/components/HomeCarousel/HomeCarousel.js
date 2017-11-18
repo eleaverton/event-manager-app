b4e6ea16a81567829be59f9073848bf7ae9bd16a
LeahStep77 <fanaticalbotanical@yahoo.com>
2017-11-18 13:55:55 -0600

    added react-bootstrap

--- client/src/components/HomeCarousel/HomeCarousel.js
+++ client/src/components/HomeCarousel/HomeCarousel.js
@@ -1,41 +1,42 @@
 import React from "react";
+import Bootstrap from "react-bootstrap";
 
 const HomeCarousel = () => (
   <div>
 
 
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <div id="myCarousel" className="carousel slide" data-ride="carousel">
      
-      <ol class="carousel-indicators">
+      <ol className="carousel-indicators">
         <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
         <li data-target="#myCarousel" data-slide-to="1"></li>
         <li data-target="#myCarousel" data-slide-to="2"></li>
       </ol>
-      <div class="carousel-inner" role="listbox">
-        <div class="item active">
-          <img class="first-slide" src="" alt="First slide" />
-          <div class="container">
-            <div class="carousel-caption">
+      <div className="carousel-inner" role="listbox">
+        <div className="item active">
+          <img className="first-slide" src="" alt="First slide" />
+          <div className="container">
+            <div className="carousel-caption">
               <h1>Example headline.</h1>
               <p>Note: If you're viewing this page via a file URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
              
             </div>
           </div>
         </div>
-        <div class="item">
-          <img class="second-slide" src="" alt="Second slide" />
-          <div class="container">
-            <div class="carousel-caption">
+        <div className="item">
+          <img className="second-slide" src="" alt="Second slide" />
+          <div className="container">
+            <div className="carousel-caption">
               <h1>Another example headline.</h1>
               <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
               
             </div>
           </div>
         </div>
-        <div class="item">
-          <img class="third-slide" src="" alt="Third slide" />
-          <div class="container">
-            <div class="carousel-caption">
+        <div className="item">
+          <img className="third-slide" src="" alt="Third slide" />
+          <div className="container">
+            <div className="carousel-caption">
               <h1>One more for good measure.</h1>
               <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit. </p>
              
@@ -43,17 +44,17 @@ const HomeCarousel = () => (
           </div>
         </div>
       </div>
-      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
+      <a className="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span className="sr-only">Previous</span>
       </a>
-      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
+      <a className="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span className="sr-only">Next</span>
       </a>
     </div>
-      <div class="container">
-      <div class="row">
+      <div className="container">
+      <div className="row">
         <div id="statBox" class="col-md-6 col-md-offset-3">
           <h1>HELLO HELLO</h1>
         </div>
