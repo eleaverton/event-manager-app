eacc9350d7224e4893c84f729b22df6a1a353d9e
Danny Nhoisaykham <danny@MBP.local>
2017-11-21 14:19:23 -0600

    add logout button and test authentication page

---
+++ client/src/components/LogoutButton.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Auth from "../modules/Auth";
+import {Button} from "react-bootstrap";
+
+class LogoutButton extends Component {
+    constructor() {
+        super()
+        this.LogUserOut = this.LogUserOut.bind(this);
+    }
+
+    LogUserOut(e) {
+        e.preventDefault();
+
+        console.log("logging user out");
+        Auth.deauthenticateUser();
+    }
+
+    render() {
+        return (
+            <Button onClick={this.LogUserOut}>Logout Button</Button>
+        );
+    }
+}
+
+export default LogoutButton;
