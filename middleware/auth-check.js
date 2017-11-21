2aa8ad6a08258b1d345a7b134eaf3f50aaef56f9
Danny Nhoisaykham <danny@MBP.local>
2017-11-20 23:56:20 -0600

    fix User not verified issue

--- middleware/auth-check.js
+++ middleware/auth-check.js
@@ -1,7 +1,6 @@
 const jwt = require("jsonwebtoken");
-// const User = require("mongoose").model("User");
 const config = require("../config");
-const db = require("../models");
+const User = require("../models/user");
 
 /**
  *  The Auth Checker middleware function.
@@ -13,8 +12,7 @@ module.exports = (req, res, next) => {
   }
 
   // get the last part from a authorization header string like "bearer token-value"
-  const token = req.headers.authorization.split(" ")[1];
-  console.log(token);
+  const token = req.headers.authorization;
 
   // decode the token using a secret key-phrase
   return jwt.verify(token, config.jwtSecret, (err, decoded) => {
@@ -23,11 +21,10 @@ module.exports = (req, res, next) => {
       console.log("Token error");
       return res.status(401).end();
     }
-
     const userId = decoded.sub;
 
     // check if a user exists
-    return db.User.findById(userId, (userErr, user) => {
+    return User.findById(userId, (userErr, user) => {
       if (userErr || !user) {
         console.log("User error");
         return res.status(401).end();
