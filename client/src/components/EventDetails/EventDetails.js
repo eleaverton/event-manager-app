b9285dbd7e55e9493d014952e79408ceaf1e1fb7
Nancy <Nancy@nancy-oconnors-macbook-pro.local>
2017-12-02 13:54:36 -0600

    fixed styling for navbar and left-align text for event description

--- client/src/components/EventDetails/EventDetails.js
+++ client/src/components/EventDetails/EventDetails.js
@@ -29,7 +29,7 @@ export class EventDetails extends Component{
                 <h3>{this.props.data[0].time}</h3>
                 <h3>{this.props.data[0].location}</h3>
                 <h4> Hosted by: {this.props.data[0].organizer.name}</h4>
-                <p>{this.props.data[0].description}</p>
+                <p className="eventDescription">{this.props.data[0].description}</p>
             	
 			</div>
 			<EventRegistrationForm eventId={this.props.data[0]._id} specificFields={this.props.data[0].specificFields} />
